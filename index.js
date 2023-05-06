6b8b02608774ba1d29983067d111dd90bd98cc4a
zo0ozzz <121275303+zo0ozzz@users.noreply.github.com>
2023-05-06 22:38:57 +0900

    캐러셀 다음에 다시 한 번

--- index.js
+++ index.js
@@ -5,8 +5,39 @@
 // function button1Handler() {
 
 // }
-
+const container = document.querySelector(".container .second");
 const one = document.querySelector("#one");
+const two = document.querySelector("#two");
+let count = 0;
+
 one.addEventListener("click", oneHandler);
 
-function oneHandler() {}
+function oneHandler() {
+  count--;
+  console.log(count);
+
+  if (count === -3) {
+    container.classList.remove("transition");
+    container.style.transform = `translate(0px, 0px)`;
+    count = 0;
+  } else {
+    container.classList.add("transition");
+    container.style.transform = `translate(${100 * count}px, 0px)`;
+  }
+}
+
+two.addEventListener("click", twoHandler);
+
+function twoHandler() {
+  count++;
+  console.log(count);
+
+  if (count === 1) {
+    container.classList.remove("transition");
+    container.style.transform = `translate(-200px, 0px)`;
+    count = -2;
+  } else {
+    container.classList.add("transition");
+    container.style.transform = `translate(${100 * count}px, 0px)`;
+  }
+}
