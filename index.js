7ac95cb80b427a1eb24919dc8fc3693fc8bd2de5
zo0ozzz <121275303+zo0ozzz@users.noreply.github.com>
2023-05-06 20:33:34 +0900

    데탑으로

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-$("#button1").on("click", buttonHandler);
+document
+  .querySelector("#submit")
+  .addEventListener("click", submitButtonHandler);
 
-function buttonHandler() {
-  $(".hello").toggleClass("end");
+function submitButtonHandler(e) {
+  e.preventDefault();
+  const inputValue = document.querySelector(".one").value;
+  console.log(inputValue);
+  if (!/\S+@\S+\.\S+/.test(inputValue)) {
+    console.log(/\S+@\S+\.\S/.test(inputValue));
+    alert("이메일 형식이 올바르지 않습니다.");
+    return;
+  }
 }
