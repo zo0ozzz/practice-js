b6a4b8282e9f8a6ab9db6dbddd5a416650c32d99
[SW4/2반] 정웅비 <zo0qzzz@gmail.com>
2023-07-21 02:54:55 +0900

    quill

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-const box = document.querySelector("#box");
-
-const regex = /l(.*?)r/gs;
-
-const html = `하하하<b>하하하</b>
-l
-<p><b>하하하</b></p>
-r`;
-
-const filtered = html.replace(regex, (match, p1) => {
-  const escaped = p1.replace(/</g, "&lt;").replace(/>/g, "&gt;");
-  return `${escaped}`;
+const quill = new Quill("#editor", {
+  theme: "snow",
 });
-
-box.innerHTML = filtered;
